b96ae8ecc7b10166b7c0fe072b7e0c581f704f3c
Duy Tran <tphduy@gmail.com>
2023-01-24 09:33:20 +0700

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by Duy Tran on 24/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
